f6f050552cafade9398f941995ab4c6620c2201e
MexpressoBean <kingk102@gmail.com>
2021-12-31 15:14:54 -0800

    enabled flash messages for login/logout

--- app/assets/javascripts/uiMenuFunction.js
+++ app/assets/javascripts/uiMenuFunction.js
@@ -2,4 +2,11 @@
   document.addEventListener('turbolinks:load', function(){
     const dropDownMenuItem = $('.ui.dropdown');
     dropDownMenuItem.dropdown();
+
+    $('.message .close')
+    .on('click', function() {
+      $(this)
+        .closest('.message')
+        .transition('fade');
+    });
 });
