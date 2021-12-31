a6ff2913c27227199decd4f06eef3a28f880d7f6
MexpressoBean <kingk102@gmail.com>
2021-12-31 14:51:03 -0800

    added actions for sessions controller and updated views for login/logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,4 +3,22 @@ class SessionsController < ApplicationController
     def new
         
     end
+
+    def create
+        user = User.find_by(username: params[:session][:username])
+        if user && user.authenticate(params[:session][:password])
+            session[:user_id] = user.id
+            flash[:success] = "#{user.username} successfully logged in"
+            redirect_to root_path
+        else
+            flash.now[:error] = "Username or password is incorrect"
+            render 'new'
+        end
+    end
+
+    def destroy
+        session[:user_id] = nil
+        flash[:success] = "Successfully logged out"
+        redirect_to login_path
+    end
 end
