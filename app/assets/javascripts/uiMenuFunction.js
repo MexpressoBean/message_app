c17d3831e5ab66358588b032f7f2c4338f1ae67b
MexpressoBean <kingk102@gmail.com>
2022-01-01 10:34:42 -0800

    changed the amount of initial messages displayed in the chat

--- app/assets/javascripts/uiMenuFunction.js
+++ app/assets/javascripts/uiMenuFunction.js
@@ -1,19 +1,29 @@
-  scrollBottom = function(){
+  scrollBottom = function() {
     if( $('#messages').length > 0){
-      $('#messages').scrollTop($('#messages')[0].scrollHeight);
+      $('#messages').scrollTop($('#messages')[0].scrollHeight)
     }
-  };
+  }
+
+  submitMessage = function() {
+    $('#message_body').on('keydown', function(e) {
+      if(e.keyCode == 13) {
+        $('button').click()
+        e.target.value = ""
+      }
+    })
+  }
 
   document.addEventListener('turbolinks:load', function(){
-    const dropDownMenuItem = $('.ui.dropdown');
-    dropDownMenuItem.dropdown();
+    const dropDownMenuItem = $('.ui.dropdown')
+    dropDownMenuItem.dropdown()
 
     $('.message .close')
     .on('click', function() {
       $(this)
         .closest('.message')
-        .transition('fade');
+        .transition('fade')
     });
 
-    scrollBottom();
+    submitMessage()
+    scrollBottom()
 });
